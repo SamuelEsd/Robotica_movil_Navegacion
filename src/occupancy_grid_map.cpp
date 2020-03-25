0742d4ea5be23b2a9adace1c0227e55a4c33820c
mr_s_p <mr_s_p@ciencias.unam.mx>
2020-03-25 09:48:01 -0600

    Todas las mesas, persiste error de tamaños al parecer

--- src/occupancy_grid_map.cpp
+++ src/occupancy_grid_map.cpp
@@ -314,6 +314,49 @@ int main( int argc, char** argv )
 
   // %EndTag(MARKER_INIT)%
 
+
+    // %Tag(MARKER_INIT)%
+  visualization_msgs::Marker marker4;
+  // Set the frame ID and timestamp.  See the TF tutorials for information on these.
+  marker4.header.frame_id = "/odom";
+  marker4.header.stamp = ros::Time::now();
+
+  // Set the namespace and id for this marker.  This serves to create a unique ID
+  // Any marker sent with the same namespace and id will overwrite the old one
+  marker4.ns = "table_marker_4";
+  marker4.id = 4;
+
+  // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
+  marker4.type = visualization_msgs::Marker::CUBE;
+
+  // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+  marker4.action = visualization_msgs::Marker::ADD;
+
+  // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+  marker4.pose.position.x = (WIDTH_unit*0.3)*5.5;
+  marker4.pose.position.y = (HEIGHT_unit*0.3)*4;
+  marker4.pose.position.z = 0.6;
+  marker4.pose.orientation.x = 0.0;
+  marker4.pose.orientation.y = 0.0;
+  marker4.pose.orientation.z = 0.0;
+  marker4.pose.orientation.w = 1.0;
+
+  // Set the scale of the marker -- 1x1x1 here means 1m on a side
+  marker4.scale.x = WIDTH_unit*0.3;
+  marker4.scale.y = HEIGHT_unit*0.3*4;
+  marker4.scale.z = 1.0;
+
+  // Set the color -- be sure to set alpha to something non-zero!
+  marker4.color.r = 0.0f;
+  marker4.color.g = 1.0f;
+  marker4.color.b = 0.0f;
+  marker4.color.a = 1.0;
+
+  marker4.lifetime = ros::Duration();
+
+
+  // %EndTag(MARKER_INIT)%
+
   
   while (ros::ok())
     {
@@ -322,6 +365,7 @@ int main( int argc, char** argv )
       marker_pub.publish(marker1);
       marker_pub.publish(marker2);
       marker_pub.publish(marker3);
+      marker_pub.publish(marker4);
       // %Tag(SLEEP_END)%
       ros::spinOnce();
       r.sleep();
